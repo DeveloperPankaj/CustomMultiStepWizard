f56d95d25a481d3d949dc2789f8be4fe773125e5
pankaj.saxena <pankaj.saxena@gingerwebs.in>
2021-06-24 11:27:02 +0530

    Adding the project

---
+++ CustomMultiStepWizard/Form1.Designer.cs
@@ -0,0 +1,241 @@
+﻿
+namespace CustomMultiStepWizard
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblChoose = new System.Windows.Forms.Label();
+            this.lblConfirm = new System.Windows.Forms.Label();
+            this.lblPayment = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnNext = new System.Windows.Forms.Button();
+            this.btnBack = new System.Windows.Forms.Button();
+            this.panelContainer = new System.Windows.Forms.Panel();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.lblDone = new System.Windows.Forms.Label();
+            this.panelContainer.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblChoose
+            // 
+            this.lblChoose.AutoSize = true;
+            this.lblChoose.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblChoose.Location = new System.Drawing.Point(140, 54);
+            this.lblChoose.Name = "lblChoose";
+            this.lblChoose.Size = new System.Drawing.Size(119, 32);
+            this.lblChoose.TabIndex = 1;
+            this.lblChoose.Text = "Choose";
+            // 
+            // lblConfirm
+            // 
+            this.lblConfirm.AutoSize = true;
+            this.lblConfirm.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblConfirm.Location = new System.Drawing.Point(421, 54);
+            this.lblConfirm.Name = "lblConfirm";
+            this.lblConfirm.Size = new System.Drawing.Size(104, 32);
+            this.lblConfirm.TabIndex = 3;
+            this.lblConfirm.Text = "Cofirm";
+            // 
+            // lblPayment
+            // 
+            this.lblPayment.AutoSize = true;
+            this.lblPayment.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPayment.Location = new System.Drawing.Point(742, 54);
+            this.lblPayment.Name = "lblPayment";
+            this.lblPayment.Size = new System.Drawing.Size(134, 32);
+            this.lblPayment.TabIndex = 5;
+            this.lblPayment.Text = "Payment";
+            // 
+            // label4
+            // 
+            this.label4.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label4.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label4.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label4.Location = new System.Drawing.Point(130, 68);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(600, 3);
+            this.label4.TabIndex = 6;
+            // 
+            // btnNext
+            // 
+            this.btnNext.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnNext.Location = new System.Drawing.Point(713, 495);
+            this.btnNext.Name = "btnNext";
+            this.btnNext.Size = new System.Drawing.Size(182, 57);
+            this.btnNext.TabIndex = 7;
+            this.btnNext.Text = "Next >";
+            this.btnNext.UseVisualStyleBackColor = true;
+            this.btnNext.Click += new System.EventHandler(this.btnNext_Click);
+            // 
+            // btnBack
+            // 
+            this.btnBack.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.btnBack.Location = new System.Drawing.Point(42, 495);
+            this.btnBack.Name = "btnBack";
+            this.btnBack.Size = new System.Drawing.Size(182, 57);
+            this.btnBack.TabIndex = 8;
+            this.btnBack.Text = "< Back";
+            this.btnBack.UseVisualStyleBackColor = true;
+            this.btnBack.Click += new System.EventHandler(this.btnBack_Click);
+            // 
+            // panelContainer
+            // 
+            this.panelContainer.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panelContainer.Controls.Add(this.lblDone);
+            this.panelContainer.Controls.Add(this.panel3);
+            this.panelContainer.Controls.Add(this.panel2);
+            this.panelContainer.Controls.Add(this.panel1);
+            this.panelContainer.Location = new System.Drawing.Point(-2, 132);
+            this.panelContainer.Name = "panelContainer";
+            this.panelContainer.Size = new System.Drawing.Size(944, 343);
+            this.panelContainer.TabIndex = 9;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = global::CustomMultiStepWizard.Properties.Resources.pending;
+            this.pictureBox3.Location = new System.Drawing.Point(644, 26);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(103, 84);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox3.TabIndex = 4;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::CustomMultiStepWizard.Properties.Resources.pending;
+            this.pictureBox2.Location = new System.Drawing.Point(326, 26);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(103, 84);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 2;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::CustomMultiStepWizard.Properties.Resources.current;
+            this.pictureBox1.Location = new System.Drawing.Point(42, 26);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(103, 84);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.White;
+            this.panel1.Location = new System.Drawing.Point(44, 23);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(831, 302);
+            this.panel1.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.Lime;
+            this.panel2.Location = new System.Drawing.Point(44, 24);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(834, 299);
+            this.panel2.TabIndex = 1;
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.panel3.Location = new System.Drawing.Point(44, 23);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(834, 302);
+            this.panel3.TabIndex = 2;
+            // 
+            // lblDone
+            // 
+            this.lblDone.AutoSize = true;
+            this.lblDone.Font = new System.Drawing.Font("Microsoft Sans Serif", 25.8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDone.Location = new System.Drawing.Point(31, 90);
+            this.lblDone.Name = "lblDone";
+            this.lblDone.Size = new System.Drawing.Size(131, 52);
+            this.lblDone.TabIndex = 3;
+            this.lblDone.Text = "Done";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(942, 578);
+            this.Controls.Add(this.panelContainer);
+            this.Controls.Add(this.btnBack);
+            this.Controls.Add(this.btnNext);
+            this.Controls.Add(this.lblPayment);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.lblConfirm);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.lblChoose);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label4);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panelContainer.ResumeLayout(false);
+            this.panelContainer.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label lblChoose;
+        private System.Windows.Forms.Label lblConfirm;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label lblPayment;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnNext;
+        private System.Windows.Forms.Button btnBack;
+        private System.Windows.Forms.Panel panelContainer;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Label lblDone;
+    }
+}
+
---
+++ CustomMultiStepWizard/Form1.cs
@@ -0,0 +1,133 @@
+﻿using CustomMultiStepWizard.Properties;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace CustomMultiStepWizard
+{
+    public partial class Form1 : Form
+    {
+        int i = 1;
+        public Form1()
+        {
+            InitializeComponent();
+            panel1.Visible = true;
+            panel2.Visible = false;
+            panel3.Visible = false;
+            btnBack.Visible = false;
+           // lblConfirm.Enabled = false;
+            //lblPayment.Enabled = false;
+            pictureBox2.Enabled = false;
+            pictureBox3.Enabled = false;
+            lblDone.Visible = false;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnNext_Click(object sender, EventArgs e)
+        {
+            i = i + 1;
+            string panelDetails = string.Empty;
+            switch(i)
+            {
+                case 1:
+                    panelDetails = "panel1";
+                    break;
+                case 2:
+                    panelDetails = "panel2";
+                    break;
+                case 3:
+                    panelDetails = "panel3";
+                    break;
+                case 4:
+                    panelDetails = "panelContainer";
+                    break;
+                default:
+                    break;
+            }
+            UpdateGUI(i, panelDetails,false);
+        }
+
+        private void btnBack_Click(object sender, EventArgs e)
+        {
+            i = i - 1;
+            string panelDetails = string.Empty;
+            switch (i)
+            {
+                case 1:
+                    panelDetails = "panel1";
+                    break;
+                case 2:
+                    panelDetails = "panel2";
+                    break;
+                case 3:
+                    panelDetails = "panel3";
+                    break;
+                case 4:
+                    panelDetails = "panelContainer";
+                    break;
+                default:
+                    break;
+            }
+            UpdateGUI(i, panelDetails,true);
+        }
+
+
+        public void UpdateGUI(int tabDetail, string activePanelDetails, bool isBack)
+        {
+            switch (activePanelDetails)
+            {
+                case "panel1":
+                    panel1.Visible = true;
+                    panel2.Visible = false;
+                    panel3.Visible = false;
+                    lblDone.Visible = false;
+                    btnBack.Visible = false;
+                    pictureBox1.Image = Properties.Resources.current;
+                    pictureBox2.Image = Properties.Resources.pending;
+                    btnNext.Visible = true;
+                    break;
+                case "panel2":
+                    panel1.Visible = false;
+                    panel2.Visible = true;
+                    panel3.Visible = false;
+                    pictureBox1.Image = Properties.Resources.completed;
+                    pictureBox2.Image = Properties.Resources.current;
+                    pictureBox3.Image = Properties.Resources.pending;
+                    btnBack.Visible = true;
+                    btnNext.Visible = true;
+                    break;
+                case "panel3":
+                    panel1.Visible = false;
+                    panel2.Visible = false;
+                    pictureBox2.Image = Properties.Resources.completed;
+                    pictureBox3.Image = Properties.Resources.current;
+                    panel3.Visible = true;
+                    btnBack.Visible = true;
+                    btnNext.Visible = true;
+                    break;
+                case "panelContainer":
+                    panel1.Visible = false;
+                    panel2.Visible = false;
+                    pictureBox3.Image = Properties.Resources.completed;
+                    lblDone.Location = new Point((panelContainer.Width / 2) - lblDone.Width, (panelContainer.Height / 2) - lblDone.Height);
+                    lblDone.Visible = true;
+                    panel3.Visible = false;
+                    btnBack.Visible = true;
+                    btnNext.Visible = false;
+                    break;
+                default:
+                    i = 0;
+                    break;
+            }
+        }
+    }
+}
